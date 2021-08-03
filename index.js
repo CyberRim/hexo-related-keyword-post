3c1454bb6e4c96ff1b5a1e793afe15a9f2c64101
CyberRim <cyberrim@foxmail.com>
2021-08-04 01:07:17 +0800

    完善功能，修补了删除或修改文章引发的错误，增加了在md中直接指定keywords字段的功能,不再将生成的keywords字段侵入式地写入到post文件的front中，仅仅保存在根目录，并在每一次hexo g时覆盖重新生成。

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /*jshint esversion: 6 */
+const util = require('./lib/util.js');
 (function() {
 	'use strict';
+	//清除文件内容
+	util.writeKeywordsFile({});
+	
 	const priority = 20;
 	hexo.extend.filter.register('before_post_render', require('./lib/keywords'),priority);
 	hexo.extend.filter.register('before_post_render', require('./lib/related'),priority+1);
